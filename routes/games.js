24255ca21214241090f71d224f9022b8480c69e1
lightsage88 <adrian.e.rosales@gmail.com>
2022-04-25 00:30:57 -0700

    okay, now I'm going to stop, for realsies

--- routes/games.js
+++ routes/games.js
@@ -9,17 +9,12 @@ const { apiUrl, apiKey } = require('../config');
 var wrapper = function (gamesConfig) {
     const router = express.Router();
     router.use(express.json());
-
     router.post('/', async (req, res) => {
         let gamesConfigData = gamesConfig;
-        console.log('you are making a request of the post type to /games');
-        console.log('here is your request body:');
         console.log(req.body);
         let { type, fieldOptions, gameId, collectionOfIds, searchValue } = req.body;
-
         requestFromGames(res, type, fieldOptions, gameId, collectionOfIds, searchValue, gamesConfigData);
     });
-
 
     const join = (array) => {
         let string = '';
@@ -35,48 +30,40 @@ var wrapper = function (gamesConfig) {
     }
 
     const requestFromGames = async (res, type, fieldOptions, gameId, collectionOfIds, searchValue, gamesConfig) => {
+        let response;
         console.log('proxy req from games running', gamesConfig);
         const igdbClient = igdb(process.env.IGDB_CLIENT_ID, gamesConfig.accessToken);
         console.log(fieldOptions);
-        let url = `${apiUrl}/games/`;
-        let data = '';
-        let headers = { 'user-key': `${apiKey}`, 'accept': 'application/json' };
         let fieldData = fieldOptions.length > 0 ? join(fieldOptions) : '';
         // let fieldData = 'name,id';
         switch (type) {
             case 'search':
-                url += `?search=${searchValue}&fields=${fieldData}`;
-                const response = await igdbClient 
+                response = await igdbClient 
                 .fields(fieldData)
                 .search(searchValue)
                 .request('/games')
                 console.log('the fucking response!', response);
                 break;
+                //TODO: See if these ones work, actually.
             case 'specificGame':
-                data = `\nfields ${fieldData}; where id=${gameId};`;
+                response = await igdbClient
+                .fields(fieldData)
+                .where(`id = ${gameID}`)
+                .request('/games')
+                // data = `\nfields ${fieldData}; where id=${gameId};`;
+                console.log('specificGameResponse:', specificGameResponse);
                 break;
             case 'franchiseGames':
-                data = `\nfields ${fieldData}; where id=(${collectionOfIds.toString()});`;
+                response = await igdbClient
+                .fields(fieldData)
+                .where(`id = (${collectionOfIds.toString()})`)
+                .request('/games')
+                // data = `\nfields ${fieldData}; where id=(${collectionOfIds.toString()});`;
                 break;
             default:
                 return;
         }
-
-        // axios({
-        //     method: "POST",
-        //     url,
-        //     headers,
-        //     data
-        // })
-        //     .then(response => {
-        //         res.json(response.data);
-        //     })
-
-        //     .catch(err => {
-        //         console.log('shit');
-        //         // handleSearchResponse('', 'error')
-        //         console.error(err);
-        //     });
+        res.json(response);
     }
     return router;
 }
