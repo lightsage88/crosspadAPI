132db0a0ec9119dc39f1a255b6dea824be012119
lightsage88 <adrian.e.rosales@gmail.com>
2022-04-24 22:00:46 -0700

    put server.js code into index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const gamesRouter = require('./routes/games');
+const coversRouter = require('./routes/covers');
+const collectionsRouter = require('./routes/collections');
+     
+      
+    
+const app = express();     
+app.use(cors());         
+// app.use(bod yParser.urlencoded({extended: false})); 
+app.use(express.json());  
+     
+
+ 
+app.use('/games', gamesRouter); 
+app.use('/collections', collectionsRouter);
+app.use('/covers', coversRouter);
+  
+app.get('/', (req, res)=>{
+    console.log('we are listening on shit');
+})
+ 
+app.listen(process.env.PORT || 8888, ()=> {
+    console.log('howdy we are on ' + 8888);
+});
+  
