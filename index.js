3579b22a998b8c469d48b730f66a69a657d96a15
lightsage88 <adrian.e.rosales@gmail.com>
2022-04-26 23:09:19 -0700

    added netlify link

--- index.js
+++ index.js
@@ -25,7 +25,18 @@
     }
     accessToken = await generateAccessToken();
     const app = express();
-    app.use(cors());
+    const whitelist = ['http://localhost:3000', 'https://infinite-harbor-38259.herokuapp.com', 'https://6268dca1c1a82460220d996d--rad-dieffenbachia-a55103.netlify.app'];
+    const corsOptions = {
+        origin: function (origin, callback) {
+            if (!origin || whitelist.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        credentials: true
+    }
+    app.use(cors(corsOptions));
     app.use(express.json());
     app.use('/games', gamesRouter({ accessToken }));
     app.use('/collections', collectionsRouter({ accessToken }));
